5995f6d16e225f705b467581a4826bf9fcecac93
Swapnil Tiwari <swaptiwari@outlook.com>
2022-05-07 18:23:29 +0530

    init

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+
+let year = new Date().getFullYear()
+function Footer()
+{
+    return (
+        <footer>
+            <p>Copyright {year}</p>
+        </footer>
+    )
+
+}
+
+export default Footer;
